0d125d421a2f120043d3986f806c98b6c32e4638
kuuku123 <dohyun0047@gmail.com>
2022-04-26 09:49:35 +0900

    rendering teamPage only if team got the valid object

---
+++ frontend/src/pages/TeamPage.tsx
@@ -0,0 +1,56 @@
+import MatchDetailCard from "components/MatchDetailCard";
+import MatchSmallCard from "components/MatchSmallCard";
+import { useEffect, useState } from "react";
+import { json } from "stream/consumers";
+import { StringLiteralType } from "typescript";
+
+interface Match {
+    id: number;
+    city: string;
+    date: Date;
+    playerOfMatch: string;
+    venue: string;
+    team1: string;
+    team2: string;
+    tossWinner: string;
+    tossDecision: string;
+    matchWinner: string;
+    result: string;
+    resultMargin: string;
+    umpire1: string;
+    umpire2: string;
+}
+interface Team {
+    id: number;
+    teamName: string;
+    totalMatches: number;
+    totalWins: number;
+    league_matches: Match[];
+}
+
+const TeamPage = () => {
+    const [team, setTeam] = useState<Team>();
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const response = await fetch(
+                "http://localhost:8080/team/Chennai Super Kings"
+            );
+            const data = await response.json();
+            setTeam(data);
+            console.log(data);
+        };
+        fetchMatches();
+    }, []);
+
+    return !team ? null : (
+        <div className="TeamPage">
+            <h1>{team!.teamName}</h1>
+            <MatchDetailCard />
+            <MatchSmallCard />
+            <MatchSmallCard />
+            <MatchSmallCard />
+        </div>
+    );
+};
+
+export default TeamPage;
